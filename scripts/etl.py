82d5cfd23900871b8259c54469baeb94cd2a6e4f
Mrityunjay Pathak <themrityunjaypathak@gmail.com>
2025-11-03 23:58:18 +0530

    feat: set up logging for easier debugging and monitoring

--- scripts/etl.py
+++ scripts/etl.py
@@ -6,3 +6,13 @@ import sys
 import logging
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, text, inspect
+
+# Setting up Logging
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+log_file = os.path.join(LOG_DIR, "etl.log")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S",
+                    handlers=[
+                        logging.FileHandler(log_file, mode="a"),
+                        logging.StreamHandler()
+                    ])
