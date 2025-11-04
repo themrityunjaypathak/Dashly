312ebe5bf0ba629b97a60d2d7959c708b9d49ce5
Mrityunjay Pathak <themrityunjaypathak@gmail.com>
2025-11-04 08:52:32 +0530

    feat: set up logging for easier debugging and monitoring

--- scripts/create_views.py
+++ scripts/create_views.py
@@ -5,3 +5,15 @@ import os
 import logging
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
+
+# ---------------- Configuration ---------------- 
+
+# Setting up Logging
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+log_file = os.path.join(LOG_DIR, "create_views.log")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S",
+                    handlers=[
+                        logging.FileHandler(log_file, mode="a"),
+                        logging.StreamHandler()
+                    ])
