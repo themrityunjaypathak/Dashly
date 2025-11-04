72e889b999d0b6c49a9286628ae085fad864a847
Mrityunjay Pathak <themrityunjaypathak@gmail.com>
2025-11-04 09:42:09 +0530

    feat: add initial library imports in generate_data script

---
+++ scripts/generate_data.py
@@ -0,0 +1,8 @@
+# ---------------- Loading Python Packages ---------------- 
+
+# Importing Libraries
+import os
+import sys
+import logging
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
